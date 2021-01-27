584c0a9cb772a84890fdffa19eee0868d8d88c15
liyuan789 <54245792+liyuan789@users.noreply.github.com>
2021-01-27 14:29:52 -0800

    Recursion

--- 509.FIbonacci_Number.py
+++ 509.FIbonacci_Number.py
@@ -1,9 +1,9 @@
 def fib(n):
-    if n <= 0:
+    if n <= 0: 
         return 0
     if n == 1:
         return 1
-    return fib(n-1) + fib(n-2)
+    return fib(n-1) + fib(n-2)    # F(n) = F(n-1) + F(n-2)
 
 
 print(fib(0))
