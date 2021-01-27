6bdd13e7ee334299b88e8f965c74aec978878305
liyuan789 <54245792+liyuan789@users.noreply.github.com>
2021-01-27 14:31:06 -0800

    Binary Search

--- 704.Binary_Search.py
+++ 704.Binary_Search.py
@@ -1,6 +1,6 @@
 # Given a sorted (in ascending order) integer array nums of n elements and a target value,
 # write a function to search target in nums.
-# If target exists, then return its index, otherwise return -1.
+# If target exists, return its index, otherwise return -1.
 
 def search(nums, target) -> int:
     if nums is None or len(nums) == 0:
