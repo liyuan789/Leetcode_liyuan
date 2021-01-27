120ed629cd898710ed0c2293882b1b3faf45a4bd
liyuan789 <54245792+liyuan789@users.noreply.github.com>
2021-01-27 14:31:33 -0800

    Binary Search

--- 74.Search_2D_matrix.py
+++ 74.Search_2D_matrix.py
@@ -1,6 +1,6 @@
 # Write an efficient algorithm that searches for a value in an m x n matrix.
 
-# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]; target = 13
 # Output: false
 
 def searchMatrix(matrix, target):
