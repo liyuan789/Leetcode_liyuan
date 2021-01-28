1c1cb6fbcb679cbace14f6914bf5640161bd95d1
liyuan789 <54245792+liyuan789@users.noreply.github.com>
2021-01-28 13:56:19 -0800

    Sorting Algorithms

---
+++ 912.Sort_Array_MergeSort.py
@@ -0,0 +1,49 @@
+
+def MergeSort(array):
+    if array is None or len(array) <= 1:
+        return array
+    return mergeSort(array, 0, len(array) - 1)
+
+
+# Split array into two sub-arrays and mergeSort both sides separately
+def mergeSort(array, start, end):
+    # Base Case
+    if start == end:
+        return [array[start]]
+
+    # Recursion Rule
+    mid = start + (end - start) // 2
+    left_sorted = mergeSort(array, start, mid)
+    right_sorted = mergeSort(array, mid + 1, end)
+
+    return merge(left_sorted, right_sorted)
+
+
+def merge(left_sorted, right_sorted):
+    result = []
+    left_idx = 0
+    right_idx = 0
+
+    # 两边都有剩余; 谁小移谁
+    while left_idx < len(left_sorted) & right_idx < len(right_sorted):
+        if left_sorted[left_idx] <= right_sorted[right_idx]:
+            result.append(left_sorted[left_idx])
+            left_idx += 1
+        else:
+            result.append(right_sorted[right_idx])
+            right_idx += 1
+
+    # 左半边有剩余
+    while left_idx < len(left_sorted):
+        result.append(left_sorted[left_idx])
+        left_idx += 1
+
+    # 右半边有剩余
+    while right_idx < len(right_sorted):
+        result.append(right_sorted[right_idx])
+        right_idx += 1
+
+    return result
+
+
+print(MergeSort([5, 2, 3, 1]))
